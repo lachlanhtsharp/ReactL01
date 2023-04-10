aa0bf99e0ee10baf4a2a3322b21d58f4485c6334
Lachlan Sharp <lachlanhtsharp@gmail.com>
2023-04-09 20:32:45 -0400

    Staging

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import ReactDOM from 'react-dom';
 
-React.createElement('h1', {}, 'Hello World!');
+React.createElement('div', {}, React.createElement('h1', {}, 'Hello World!'));
+
+const GroceryListFtn = props => {
+    return (
+        <ul>
+            <li>{props.item1}</li>
+            <li>{props.item2}</li>
+            <li>{props.item3}</li>
+        </ul>
+    )
+}
+
+let groceryList = (
+    <ul>
+        <li>Apples</li>
+        <ul>
+            <li>$1.00 per lb</li>
+        </ul>
+        <li>Oranges</li>
+        <ul>
+            <li>$0.70 per lb</li>
+        </ul>
+        <li>Mangos</li>
+        <ul>
+            <li>$2.00 per lb</li>
+        </ul>
+    </ul>
+)
 
 ReactDOM.render(
-    React.createElement('h1', {}, 'Hello World!'),
+    <GroceryListFtn item1="Apples" item2="Oranges" item3="Mangos" />
+
+    ,
     document.getElementById('root')
 )
